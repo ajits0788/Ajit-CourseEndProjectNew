eea06d25d51c5f8e943cc89682c4af71b8b06e08
Ajit <ajit.singh@123>
2025-07-20 11:24:24 -0400

    Second Commit

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "DATA PAGE";
+        return "Ajit DataPage";
     }
 
     @GetMapping("/message")
     public String getMessage() {
-        return "MESSAGE PAGE";
+        return "Ajit Message";
     }
 }
